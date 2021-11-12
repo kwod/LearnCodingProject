614800a03835e3bcd7a24d540a7b930202e64d1d
kwod <kwd@gmx.de>
2021-11-12 12:16:31 +0100

    update mysqrt

--- MySqrt.jl
+++ MySqrt.jl
@@ -1,16 +1,23 @@
-function mysqrt(x, N = 10)
-    t = 1
-    for _ = 1:N
-        t = (t + x/t)/2
-    end
-    return t
-end
-
-(mysqrt(3), √3)
-
 using Plots
 
-fs = [x -> mysqrt(x, i) for i in 1:5]
+function mysqrt(x, N)
+    t = 1
+    for i in 1:N
+        t = (t + x/t)/2
+    end
+    t
+end
 
-plot(range(0,50; length = 500), [fs..., √],
+function mysqrt(x)
+    t₀, t₁ = one(x), x
+    while t₀ ≠ t₁
+        t₀, t₁ = t₁, (t₁ + x/t₁)/2
+    end
+    t₁
+end
+
+mysqrt(3)
+
+plot(range(0,50; length = 500),
+    [[x -> mysqrt(x, i) for i in 1:5]..., √],
     label = hcat(["Iteration $i" for i in 1:5]..., "√"))
